f3d72e81ea8d8f183eb4378cabb3f029c06790b4
ELias <elias2005new@gmail.com>
2026-09-26 21:47:23 +0300

    nothing new

--- src/generator.py
+++ src/generator.py
@@ -3,8 +3,18 @@ import numpy as np
 import json
 
 
-def json_format_next_ids(input_ids: list[int], id_to_token: dict) -> list[int]:
-    pass
+def json_format_next_ids(
+        input_ids: list[int], logits: list[float],
+          id_to_token: dict, json_data: dict,
+            prompt_len: int, model
+        ) -> list[float]:
+    if len(input_ids) == prompt_len:
+        logits[:] = -np.inf
+        logits[json_data["{"]] = 0.0
+        return logits
+    else:
+        generate_id = input_ids[prompt_len:]
+        
 
 
 def generate_text(prompt: str, MAX_TOKEN: int = 30):
@@ -15,14 +25,15 @@ def generate_text(prompt: str, MAX_TOKEN: int = 30):
     with open(voc_path, 'r', encoding="utf-8") as file:
         json_data = json.load(file)
         id_to_token = {value: key for key, value in json_data.items()}
-
+    print(json_data["{"])
     if hasattr(raw_encoded, "tolist"):
         input_ids = raw_encoded.tolist()[0]
     else:
         input_ids = raw_encoded[0]
-
+    input_len = len(input_ids)
     for _ in range(MAX_TOKEN):
         logits = model.get_logits_from_input_ids(input_ids)
+        logits = json_format_next_ids(input_ids, logits, id_to_token, json_data, input_len, model)
         next_input_id = int(np.argmax(logits))
         input_ids.append(next_input_id)
         new_text = model.decode([next_input_id])
